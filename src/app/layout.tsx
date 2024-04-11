73f25a52e1491b94e488db415fcf0ddc072b059f
andrebellico <mhsandrevsbellico@gmail.com>
2024-04-11 20:21:18 -0300

    fix all eslint error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,25 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import HeaderPage from "@/components/Header";
+import HeaderPage from '@/components/Header'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "Plana",
-  description: "Adhoc",
-};
-
+  title: 'Plana',
+  description: 'Adhoc',
+}
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
       <body className={inter.className}>
-    <HeaderPage/>
-        {children}</body>
+        <HeaderPage />
+        {children}
+      </body>
     </html>
-  );
+  )
 }
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,11 @@
-export default function Button (props: any) {
-    return (
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            {props.children}
-        </button>
-    )  
+interface ButtonProps {
+  children: React.ReactNode
+}
+
+export default function Button({ children }: ButtonProps) {
+  return (
+    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      {children}
+    </button>
+  )
 }
