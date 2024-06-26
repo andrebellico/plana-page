437bf8a217c684429e029d6fa107994aba0e902c
andrebellico <mhsandrevsbellico@gmail.com>
2024-06-26 19:06:35 -0300

    feat: Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,27 @@
-import type { Metadata } from 'next'
-import { Bitter } from 'next/font/google'
-import './globals.css'
-import { Providers } from './providers'
-const bitter = Bitter({ subsets: ['latin'] })
+import type { Metadata } from "next";
+import { Bitter } from "next/font/google";
+import "./globals.css";
+import { Providers } from "./providers";
+import { Analytics } from "@vercel/analytics/react";
+
+const bitter = Bitter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Plana Adhoc',
-}
+  title: "Plana Adhoc",
+};
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <body className={bitter.className}>
-        <Providers>{children}</Providers>
+        <Providers>
+          {children}
+          <Analytics />
+        </Providers>
       </body>
     </html>
-  )
+  );
 }
